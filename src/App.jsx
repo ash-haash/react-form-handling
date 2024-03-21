34c934195afa1ee800922317853e7b6fb31f6d2d
ashrafur rahman farhad <itsfaarhaad@gmail.com>
2024-03-21 16:10:07 +0000

    form using react useRef() hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 import SimpleForm from './Components/SimpleForm/SimpleForm'
 import StatefulForm from './Components/StatefulForm/StatefulForm'
+import UseRefForm from './Components/useRefForm/UseRefForm'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
       <h2>React Form Handling</h2>
       <br />
-      <StatefulForm></StatefulForm>
+      <UseRefForm></UseRefForm>
+      {/* <StatefulForm></StatefulForm> */}
       {/* <SimpleForm></SimpleForm> */}
     </>
   )
---
+++ src/Components/useRefForm/UseRefForm.jsx
@@ -0,0 +1,32 @@
+import React, { useEffect, useRef } from 'react';
+
+const UseRefForm = () => {
+    const nameRef = useRef(null);
+    const emailRef = useRef(null);
+    const passwordRef = useRef(null);
+
+    useEffect(() => {
+        nameRef.current.focus();
+    }, [])
+
+    const handleSubmit = e => {
+        e.preventDefault();
+        console.log(nameRef.current.value, emailRef.current.value, passwordRef.current.value)
+    }
+
+    return (
+        <div>
+            <form onSubmit={handleSubmit}>
+                <input ref={nameRef} type="text" name="name" />
+                <br />
+                <input ref={emailRef} type="email" name="email" id="" />
+                <br />
+                <input ref={passwordRef} type="password" name="password" />
+                <br />
+                <input type="submit" value="Submit" />
+            </form>
+        </div>
+    );
+};
+
+export default UseRefForm;
