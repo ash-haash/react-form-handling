cd2cd895dc5f44919b5add1f6cb4c2aa38da63d3
ashrafur rahman farhad <itsfaarhaad@gmail.com>
2024-03-21 15:43:53 +0000

    form using react useState

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,17 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import SimpleForm from './Components/SimpleForm/SimpleForm'
+import StatefulForm from './Components/StatefulForm/StatefulForm'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <SimpleForm></SimpleForm>
+      <h2>React Form Handling</h2>
+      <br />
+      <StatefulForm></StatefulForm>
+      {/* <SimpleForm></SimpleForm> */}
     </>
   )
 }
